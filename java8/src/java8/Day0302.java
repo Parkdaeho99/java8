e42f5bb27abc796ec3d85705a42c0e54ef64e041
Parkdaeho <parkdaeho.dev@gmail.com>
2020-03-16 12:51:43 +0900

    3일차 기본자료형 최대값 최소값

---
+++ java8/src/java8/Day0302.java
@@ -0,0 +1,14 @@
+package java8;
+
+public class Day0302 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		Integer a = new Integer(3);
+		System.out.println("int형의 최대값: " + Integer.MAX_VALUE);
+		System.out.println("int형의 최소값: " + Integer.MIN_VALUE);
+		System.out.println("byte형의 최대값: " + Byte.MAX_VALUE);
+
+	}
+
+}
