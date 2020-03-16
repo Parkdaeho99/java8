289f0c30088b7e0dea47229e58a5f8f781f9b85e
Parkdaeho <parkdaeho.dev@gmail.com>
2020-03-16 14:09:29 +0900

    3일차 관계연산자

---
+++ java8/src/java8/Day0305.java
@@ -0,0 +1,17 @@
+package java8;
+
+public class Day0305 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int num1 = 10; int num2 = 20; int num3 = 20;
+		System.out.println("num1 < num2: " + (num1 < num2) );
+		System.out.println("num1 <= num2: " + (num1 <= num2) );
+		System.out.println("num1 > num2: " + (num1 > num2) );
+		System.out.println("num1 >= num2: " + (num1 >= num2) );
+		System.out.println("num1 == num2: " + (num1 == num2) );
+		System.out.println("num1 != num2: " + (num1 != num2) );
+		
+	}
+
+}
