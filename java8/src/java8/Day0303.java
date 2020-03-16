a6a6cab8836c3ac19869038e239dac9e289801ab
Parkdaeho <parkdaeho.dev@gmail.com>
2020-03-16 13:06:49 +0900

    3일차 변수범위

---
+++ java8/src/java8/Day0303.java
@@ -0,0 +1,15 @@
+package java8;
+
+public class Day0303 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int number1 = 100;
+		{
+			int number2 = 300;
+			System.out.println(number2);
+		}
+		
+	}
+
+}
