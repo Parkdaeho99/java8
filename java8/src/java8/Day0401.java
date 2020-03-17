1775a0090fcff8f5ee3df994e325cd928d1e4cf4
Parkdaeho <parkdaeho.dev@gmail.com>
2020-03-17 14:14:23 +0900

    비트 시프트

---
+++ java8/src/java8/Day0401.java
@@ -0,0 +1,20 @@
+package java8;
+
+public class Day0401 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int a = 3;
+		int b = 5;
+		
+		int res1 = a << 2;
+		int res2 = a >> 1;
+		System.out.println(res1);
+		System.out.println(res2);
+		
+		int res3 = ~b;
+		System.out.println(res3+1);
+
+	}
+
+}
