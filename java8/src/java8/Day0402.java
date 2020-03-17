27e7c5a5caad6f105093f4eb2c7d60339dd30af1
Parkdaeho <parkdaeho.dev@gmail.com>
2020-03-17 14:13:32 +0900

    System.in.read() - 버퍼 첫칸만 긁어온다?

---
+++ java8/src/java8/Day0402.java
@@ -0,0 +1,16 @@
+package java8;
+
+import java.io.IOException;
+
+public class Day0402 {
+
+	public static void main(String[] args) throws IOException {
+		// 입출력 관련 예외들은 IOException한테 던져서 맡겨라.
+		// TODO 자동 생성된 메소드 스텁
+		System.out.print("키보드로 입력하세요.");
+		int a = System.in.read(); //마지막 엔터까지 버퍼에 저장된다.
+		System.out.println(a);
+
+	}
+
+}
